707a96133918f754b9f8ab1544b62e16301bc8e5
kodandapanik <kkpani.sachin2gmail.com>
2014-07-10 13:07:20 +0530

    updated api url

--- lib/omniauth/strategies/myverifiedid.rb
+++ lib/omniauth/strategies/myverifiedid.rb
@@ -8,7 +8,7 @@ module OmniAuth
 
       # This is where you pass the options you would pass when
       # initializing your consumer from the OAuth gem.
-      option :client_options, {:site => "https://myverifiedid-api.com",
+      option :client_options, {:site => "https://api.myverifiedid.com",
         :authorize_path => "/oauth/authorize"
       }
 
